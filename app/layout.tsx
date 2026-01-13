2b6a3eda5d21cb55ef0534141135ef8db2db9602
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2026-01-13 16:55:15 +0800

    added google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,9 +42,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="ZmQ30Eoo_grE_3GEMiZoWyKFGhj6LdE2hxvWpUKyAFE" />
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TC7926M3');</script>
+
       </head>
       <body className={`${inter.className} font-sans antialiased`}>
         {children}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TC7926M3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Toaster position="top-right" />
       </body>
     </html>
